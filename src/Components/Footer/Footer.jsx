3d1ef4ef15b31b0870a5edf21bdd5c63793c0ad4
gokaySendil <gokaysendilw@outlook.com>
2022-08-30 02:13:12 +0300

    Story section bug fixed
    p element was exceeded the container

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-nav"></div>
+      <div className="Info-container"></div>
+    </div>
+  );
+};
+
+export default Footer;
